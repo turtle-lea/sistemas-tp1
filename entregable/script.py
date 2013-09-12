cbb58fc4a621a1a99700aededbe338724aebf639
Leandro Matayoshi <lmatayoshi@ws11.labo6>
2013-09-11 21:47:50 -0300

    agregue ready_ready.py

--- entregable/script.py
+++ entregable/script.py
@@ -5,13 +5,14 @@ import os
 
 #./simusched <lote.tsk> <num_cores> <costo_cs> <costo_mi> <sched> [<params_sched>]
 
-os.system("mkdir experimento_9")
+os.system("mkdir experimento_8")
 
 for i in range(20): 
-	os.system("./simusched loteLottery.tsk 1 0 0 SchedLottery 4 9 > test")
+	os.system("./simusched loteLottery.tsk 1 1 2 SchedLottery 16 "+str(i*i)+" > test")
 	nombre_test = "test_"+str(i)
-	os.system("cp test experimento_9/"+nombre_test)
+	os.system("cp test experimento_8/"+nombre_test)
 	os.system("python graphsched.py test")
-	os.system("cp test.png experimento_9/"+nombre_test+".png")
+	os.system("cp test.png experimento_8/"+nombre_test+".png")
+	os.system("echo Termine "+str(i))
 	
 	

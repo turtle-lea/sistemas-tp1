a5e7da092a4d05aa3139d883ba57d32a4571d71d
Alexander Nicolas Szyrej <aszyrej@ws11.labo6>
2013-11-11 20:23:24 -0300

    giles

---
+++ entregable/experimentacion/generador_lotes.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+rm -f loteCelular.tsk
+rm -f lotePC.tsk
+rm -f loteCalc.tsk
+rm -f loteRTC.tsk
+
+touch loteCelular.tsk
+touch lotePC.tsk
+touch loteCalc.tsk
+touch loteRTC.tsk
+
+#celular
+for i in `seq 1 40`;
+do
+	echo "TaskConsola 50 1 100" >> loteCelular.tsk
+done
+
+#pc
+for i in `seq 1 20`;
+do
+	echo "TaskConsola 40 1 30" >> lotePC.tsk
+	echo "TaskCPU 50" >> lotePC.tsk
+	echo "TaskBatch 50 $i" >> lotePC.tsk
+done
+
+#calculo
+for i in `seq 1 200`
+do
+	if [ $i -lt "101" ];
+	then
+		echo "TaskCPU 75" >> loteCalc.tsk
+	else
+		echo "TaskCPU 200" >> loteCalc.tsk
+
+	fi
+done
+
+
+#RTC
+for i in `seq 1 100`
+do
+	echo "TaskBatch 100 40" >> loteRTC.tsk
+done

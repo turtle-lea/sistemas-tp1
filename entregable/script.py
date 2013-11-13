0286c58ab359d8e408f47dfc91e3113d4ef2ebfb
mnsantos <martin.n.santos@gmail.com>
2013-11-13 17:39:51 -0300

    cambios a scripy y sched rr

--- entregable/script.py
+++ entregable/script.py
@@ -4,6 +4,8 @@ from matplotlib.pyplot import *
 import sys
 import time
 import os
+import math
+import matplotlib.pyplot as plt
 
 #./simusched <lote.tsk> <num_cores> <costo_cs> <costo_mi> <sched> [<params_sched>]
 os.system("rm -rf experimento")
@@ -13,6 +15,12 @@ os.system("mkdir experimento")
 ywt=[] #waiting times
 yta=[] #ta times
 x=[] #quantums
+desviosWt=[]
+desviosTa=[]
+
+#random tasks
+wtProm=[]
+taProm=[]
 
 wtMin = 1000000
 taMin = 1000000
@@ -20,73 +28,94 @@ fo = open("wtta.out", "a") #"wtta.out"
 sched = str(sys.argv[1]) #scheduler
 lote = str(sys.argv[2]) #lote de tareas
 qm = int(sys.argv[3]) #quantumLimite
-cores = str(sys.argv[4]) #cores totales
+cores = str(sys.argv[4]) #coresLimite
 
-for i in range (1,qm):
-	j = str(i)
-	print j
-	if (sched=="SchedLottery"):
-		os.system("./simusched "+lote+" "+cores+" 1 2 "+sched+" "+j+" "+j+" > test")
-	if (sched=="SchedRR" or sched=="SchedRR2"):
-		string = "./simusched "+lote+" "+cores+" 1 2 "+sched+" "
-		for h in range(0,int(cores)):
-			string = string+j+" "
-		string = string+"> test"		
-		os.system(string)
+for k in range(2,int(cores)+1):
+	for i in range (1,qm):
+		j = str(i)
+		print j
 		
-	os.system("python graphsched.py test")
-	os.system("./wtta < test > tmp")
-	
-	tmp1 = open("tmp",'r').read()
-	tmp = open("tmp",'r').readlines()
-	wt = tmp[0].split()
-	wt = float(wt[2])	
-	ta = tmp[1].split()
-	ta = float(ta[2]) 
-	
-	ywt.append(wt)
-	yta.append(ta)
-	x.append(i)
-	
-	if (taMin > ta):
-		taMin = ta
-		quantum1 = i
-	if (wtMin > wt):
-		wtMin = wt
-		quantum2 = i
+		for s in range (0,20):
+			if (sched=="SchedLottery"):
+				os.system("./simusched "+lote+" "+cores+" 1 2 "+sched+" "+j+" "+j+" > test")
+			if (sched=="SchedRR" or sched=="SchedRR2"):
+				string = "./simusched "+lote+" "+cores+" 1 2 "+sched+" "
+				for h in range(0,k):
+					string = string+j+" "
+				string = string+"> test"		
+				os.system(string)
+				
+			#os.system("python graphsched.py test")
+			os.system("./wtta < test > tmp")
+			
+			tmp1 = open("tmp",'r').read()
+			tmp = open("tmp",'r').readlines()
+			wt = tmp[0].split()
+			wt = float(wt[2])	
+			ta = tmp[1].split()
+			ta = float(ta[2])
+			
+			wtProm.append(wt) 
+			taProm.append(ta) 
+
+		wt = sum(wtProm) / float(len(wtProm))
+		ta = sum(taProm) / float(len(taProm))
+		desvioWt = [(z-wt)*(z-wt) for z in wtProm]
+		desvioWt = math.sqrt(sum(desvioWt) / (float(len(desvioWt))-1))
+		desviosWt.append(desvioWt)
+		desvioTa = [(b-ta)*(b-ta) for b in taProm]
+		desvioTa = math.sqrt(sum(desvioTa) / (float(len(desvioTa))-1))
+		desviosTa.append(desvioTa)
+
+		wtProm=[]
+		taProm=[]	
 		
-	fo.write("Quantum: "+j+"\n")
-	fo.write(tmp1)
-	nombre_test = "test_"+j
-	os.system("mv test.png experimento/"+nombre_test+".png")
-	os.system("mv test experimento/"+nombre_test+".out")
+		ywt.append(wt)
+		yta.append(ta)
+		x.append(i)
+		
+		if (taMin > ta):
+			taMin = ta
+			quantum1 = i
+		if (wtMin > wt):
+			wtMin = wt
+			quantum2 = i
+		fo.write("Quantum: "+j+"\n")
+		fo.write("Waiting time: "+str(wt)+"\n")
+		fo.write("Turnaround time: "+str(ta)+"\n")
+		#nombre_test = "test_"+j
+		#os.system("mv test.png experimento/"+nombre_test+".png")
+		#os.system("mv test experimento/"+nombre_test+".out")
+		os.system("rm test")
+		
+	os.system("rm tmp")
+	fo.write("\n")
+	fo.write("Quantum con menor waiting time: "+str(quantum2)+"("+str(wtMin)+")\n")
+	fo.write("Quantum con menor turnaround time: "+str(quantum1)+"("+str(taMin)+")")
+	#os.system("mv wtta.out experimento/")
+
+	#Graficador
+
+	plt.figure(1)
+	plt.xlabel("Quantum")
+	plt.ylabel("Waiting time")
+	plt.errorbar(x, ywt, yerr=desviosWt, fmt='.', color='black')
+	plt.axis([0,qm,0,max(ywt)+2])
+	savefig("experimento/cores_"+str(k)+"_wt.jpg")
+
+	plt.figure(2)
+	plt.xlabel("Quantum")
+	plt.ylabel("Turnaround time")
+	plt.errorbar(x, yta, yerr=desviosTa, fmt='.', color='black')
+	plt.axis([0,qm,taMin-10,max(yta)+10])
+	savefig("experimento/cores_"+str(k)+"_ta.jpg")
 	
-os.system("rm tmp")
-fo.write("\n")
-fo.write("Quantum con menor waiting time: "+str(quantum2)+"("+str(wtMin)+")\n")
-fo.write("Quantum con menor turnaround time: "+str(quantum1)+"("+str(taMin)+")")
+	wtMin = 1000000
+	taMin = 1000000	
+	ywt=[] #waiting times
+	yta=[] #ta times
+	x=[] #quantums
+	desviosWt=[]
+	desviosTa=[]
+	
 os.system("mv wtta.out experimento/")
-
-#Graficador
-
-
-
-xlabel("Quantum")
-ylabel("Waiting time")
-plot(x, ywt, 'ro')
-axis([0,qm,0,max(ywt)+2])
-savefig("experimento/wt.jpg")
-
-subplot(1,1,1)
-
-xlabel("Quantum")
-ylabel("Turnaround time")
-plot(x, yta, 'ro')
-axis([0,qm,taMin-10,max(yta)+10])
-savefig("experimento/ta.jpg")
-
-
-
-
-	
-	
